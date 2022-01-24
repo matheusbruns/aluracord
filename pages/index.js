ea59bf71098e554f502486eb2657f642a2bfec49
Matheus Bruns <matheus.rbruns@gmail.com>
2022-01-24 11:25:25 -0300

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return( 
+    <div>
+      <h1>Bem Vindo de volta!</h1>
+      <h2>Discord - Alura</h2>
+      <style jsx>{`
+      p {
+        color: red;
+      }
+      `}</style>
+    </div>
+   
+  )
 }
 
 export default HomePage
