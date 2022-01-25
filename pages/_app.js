5ae1cd79ad491cdeac52e3d08e55eb283075f313
Matheus Bruns <matheus.rbruns@gmail.com>
2022-01-25 14:27:46 -0300

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,41 @@
+// Tudo que estiver aqui vai aparecer em todas as paginas
+
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+      /* App fit Height */ 
+      html, body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */ 
+    `}</style>
+  );
+}
+
+export default function CustomApp({ Component, pageProps}) {
+  console.log('em todas as páginas')
+  return (
+    <>
+    
+    <GlobalStyle />
+    <Component {...pageProps} />
+    </>
+  );
+}
