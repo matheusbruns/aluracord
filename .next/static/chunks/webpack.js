8868ddd017cf3773dea7972a05b8929a3f179089
Matheus Bruns <matheus.rbruns@gmail.com>
2022-01-26 16:05:47 -0300

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc27d235cc04e60e"; }
+/******/ 		__webpack_require__.h = function() { return "8cf8aeea024ae211"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
