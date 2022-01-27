6206ff681119bf58b31eef495eedc9adc05244ae
Matheus Bruns <matheus.rbruns@gmail.com>
2022-01-27 15:53:59 -0300

    Tela de Chat

--- pages/chat.js
+++ pages/chat.js
@@ -1,33 +1,49 @@
 import { Box, Text, TextField, Image, Button } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
+import { createClient } from '@supabase/supabase-js'
+
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzI5MzIyOSwiZXhwIjoxOTU4ODY5MjI5fQ.-cjwp3EK2xu71Fydc7MnLNWswKLgRwaYwHa-ziaYYHs';
+const SUPABASE_URL = 'https://wcdwgthihwwjfxthaanu.supabase.co';
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 
 export default function ChatPage() {
     const [mensagem, setMensagem] = React.useState('');
     const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
 
-    /*
-    // Usuário
-    - Usuário digita no campo textarea
-    - Aperta enter para enviar
-    - Tem que adicionar o texto na listagem
-    
-    // Dev
-    - [X] Campo criado
-    - [X] Vamos usar o onChange usa o useState (ter if pra caso seja enter pra limpar a variavel)
-    - [X] Lista de mensagens 
-    */
+    React.useEffect(() => {
+        supabaseClient
+            .from('mensagens')
+            .select('*')
+            .order('id', { ascending: false}) // para as mensagens ficarem de baixo para cima
+            .then(({ data, }) => {
+                console.log(`dados da consulta: ${data}`);
+                setListaDeMensagens(data);
+            });
+    }, [listaDeMensagens]);
+
     function handleNovaMensagem(novaMensagem) {
         const mensagem = {
-            id: listaDeMensagens.length + 1,
+            //id: listaDeMensagens.length + 1,
             de: 'matheusbruns',
             texto: novaMensagem,
         };
 
-        setListaDeMensagens([
-            mensagem,
-            ...listaDeMensagens,
-        ]);
+        supabaseClient
+            .from('mensagens')
+            .insert([
+                mensagem
+            ])
+            .then(({ data }) => {
+                console.log(data)
+                setListaDeMensagens([
+                    data[0],
+                    ...listaDeMensagens,
+                ]);
+            })
+        
         setMensagem('');
     }
 
@@ -38,7 +54,7 @@ export default function ChatPage() {
                 backgroundColor: appConfig.theme.colors.primary[500],
                 backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
                 backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
-                color: appConfig.theme.colors.neutrals['000']
+                color: appConfig.theme.colors.neutrals['000'],
             }}
         >
             <Box
@@ -112,8 +128,8 @@ export default function ChatPage() {
                                 color: appConfig.theme.colors.neutrals[200],
                             }}
                         />
-                        <Button
-                          onKeyPress={(event) => { // quando clicar no enter, limpar a variavel
+                        {/* <Button
+                          onKeyPress={(event) => {
                               if (event.key === 'Enter') {
                                   event.preventDefault();
                                   handleNovaMensagem(mensagem);
@@ -131,7 +147,7 @@ export default function ChatPage() {
                             color: appConfig.theme.colors.neutrals[200],
                             cursor:'pointer',
                         }}
-                        />
+                        /> */}
                     </Box>
                 </Box>
             </Box>
@@ -200,7 +216,7 @@ function MessageList(props) {
                                     display: 'inline-block',
                                     marginRight: '8px',
                                 }}
-                                src={`https://github.com/matheusbruns.png`}
+                                src={`https://github.com/${mensagem.de}.png`}
                             />
                             <Text tag="strong">
                                 {mensagem.de}
